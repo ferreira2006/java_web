17715746715514c2ab96bc384c1b0fc1495ab921
ferreira2006 <ferreira2006@gmail.com>
2023-04-22 17:08:24 -0300

    Carga inical

---
+++ crudspringmvc/src/main/java/com/example/crudspringmvc/CrudspringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringmvcApplication.class, args);
+	}
+
+}
---
+++ crudspringmvc/src/test/java/com/example/crudspringmvc/CrudspringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
