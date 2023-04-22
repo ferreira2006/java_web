111496115a7344a2e23e072043ea905534a71784
ferreira2006 <ferreira2006@gmail.com>
2023-04-22 19:39:07 -0300

    Adicionado comentários

--- helloWorld/src/main/java/com/example/helloWorld/HelloWorldController.java
+++ helloWorld/src/main/java/com/example/helloWorld/HelloWorldController.java
@@ -8,9 +8,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
 
+	/* A anotação @GetMapping indica a url que deve ser digitada no navegador para
+	 * chamar este método ola() que retorna a página Hello http://localhost:8080/saudacao */
 	@GetMapping("/saudacao")
 	public String ola(@RequestParam(name="nome", required=false, defaultValue="World") String nome, Model model) {
+		/* O método ola pede 2 parâmetros: String nome - Model model 
+		 * a variável nome pode ser passado por quem fizer a requisição, caso negativo "world" será atribuida
+		 *  Model é um implementação de uma interface gerérica do spring framework para tranbalhar com um Model
+		 * */
+		
+		/* Adiciona um atributo composto por um nome e valor, estilo MAP (chave:valor) */
 		model.addAttribute("nome", nome);
+		
+		/* é retornado ao navegador um novo arquivo HTML (saudacao.html) com o único valor
+		 * que foi atribuido ao Model (o nome que foi encaminhado na requisição)
+		 */
 		
 		return "saudacao";
 	}
