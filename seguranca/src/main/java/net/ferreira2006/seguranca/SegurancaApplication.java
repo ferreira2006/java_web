be405a575c02e717e9ec99268cec89b4cb23382e
ferreira2006 <ferreira2006@gmail.com>
2023-05-08 21:48:52 -0300

    inicial

--- seguranca/src/main/java/net/ferreira2006/seguranca/SegurancaApplication.java
+++ seguranca/src/main/java/net/ferreira2006/seguranca/SegurancaApplication.java
@@ -13,45 +13,42 @@ public class SegurancaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SegurancaApplication.class, args);
-		
-	}
-	
-	@RestController
-	class HttpController {
-		
-		@GetMapping("/public")
-		String publicRoute() {
-			return "<h1>Plublic route, feel to lock around!</h1>";
-			
-		}
-		
-		@GetMapping("/private")
-		String privateRoute(@AuthenticationPrincipal OidcUser principal) {
-			return String.format("""
-					<h1>Private route, only authorized personal! 🔐  </h1>
-					""");
-		}
-
-		@GetMapping("/cookie")
-		String cookie(@AuthenticationPrincipal OidcUser principal) {
-			return String.format("""
-						<h1>Oauth2 🔐  </h1>
-					<h3>Principal: %s</h3>
-					<h3>Email attribute: %s</h3>
-					<h3>Authorities: %s</h3>
-					<h3>JWT: %s</h3>
-					""", principal, principal.getAttribute("email"), principal.getAuthorities(),
-					principal.getIdToken().getTokenValue());
-		}
-
-		@GetMapping("/jwt")
-		String jwt(@AuthenticationPrincipal Jwt jwt) {
-			return String.format("""
-					Principal: %s\n
-					Email attribute: %s\n
-					JWT: %s\n
-					""", jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
-		}
 	}
 
+}
+
+@RestController
+class HttpController {
+	@GetMapping("/public")
+	String publicRoute() {
+		return "<h1>Public route, feel free to look around! 🔓 </h1>";
+	}
+
+	@GetMapping("/private")
+	String privateRoute(@AuthenticationPrincipal OidcUser principal) {
+		return String.format("""
+				<h1>Private route, only authorized personal! 🔐  </h1>
+				""");
+	}
+
+	@GetMapping("/cookie")
+	String cookie(@AuthenticationPrincipal OidcUser principal) {
+		return String.format("""
+					<h1>Oauth2 🔐  </h1>
+				<h3>Principal: %s</h3>
+				<h3>Email attribute: %s</h3>
+				<h3>Authorities: %s</h3>
+				<h3>JWT: %s</h3>
+				""", principal, principal.getAttribute("email"), principal.getAuthorities(),
+				principal.getIdToken().getTokenValue());
+	}
+
+	@GetMapping("/jwt")
+	String jwt(@AuthenticationPrincipal Jwt jwt) {
+		return String.format("""
+				Principal: %s\n
+				Email attribute: %s\n
+				JWT: %s\n
+				""", jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
+	}
 }
