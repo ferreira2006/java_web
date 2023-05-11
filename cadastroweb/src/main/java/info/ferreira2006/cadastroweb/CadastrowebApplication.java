8ded23b8ef7155678376ecb00e7b68ae418f33aa
info <info@PCInfo>
2023-05-11 08:48:51 -0300

    Carga Inicial

---
+++ cadastroweb/src/main/java/info/ferreira2006/cadastroweb/CadastrowebApplication.java
@@ -0,0 +1,13 @@
+package info.ferreira2006.cadastroweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrowebApplication.class, args);
+	}
+
+}
---
+++ cadastroweb/src/test/java/info/ferreira2006/cadastroweb/CadastrowebApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ferreira2006.cadastroweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
